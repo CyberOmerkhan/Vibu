8f0f4b0b9e150a4b43145eb0e7392901df4c5dcd
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-11-03 20:49:08 -0500

    v0.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,18 @@ function App() {
         <Header />
         <div className = "intro-form">
           <form className = "intro-inputs">
-            <input type = "text" placeholder = "How many people?"></input>
-            <input type = "text" placeholder = "How much time do you have?"></input>
+            <label>
+              What's your favorite movie and why?
+              <textarea placeholder = "The Shawshank Redemption because it taught me "></textarea>
+            </label>
+            <label>
+              Are you in the mood for something new or classic?
+              <input type = "text" placeholder = "I want to watch movies that were released after 1990"></input>
+            </label>
+            <label>
+              Do you wanna have fun or do you want something serious?
+              <input type = "text" placeholder = "I want to watch something stupid and fun"></input>
+            </label>
             <input className = "form-btn" type = "submit" text = "Submit" />
           </form>
         </div>
