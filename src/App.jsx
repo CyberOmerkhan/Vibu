740a67852e1af73f12fcc07f6d1f0351a966f811
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-11-03 20:30:55 -0500

    v0.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Header from './intro-header'
 
 function App() {
     return (
-      <div class = "intro">
+      <div className = "intro">
         <Header />
-        <div class = "intro-form">
-          <form class = "intro-inputs">
+        <div className = "intro-form">
+          <form className = "intro-inputs">
             <input type = "text" placeholder = "How many people?"></input>
             <input type = "text" placeholder = "How much time do you have?"></input>
-            <input class = "form-btn" type = "submit" text = "Submit" />
+            <input className = "form-btn" type = "submit" text = "Submit" />
           </form>
         </div>
       </div>
