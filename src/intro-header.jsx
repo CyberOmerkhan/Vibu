2461e8d392ec2de5952cedc4cc42ed1f0fadd435
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-11-07 23:39:23 -0500

    v0.0.1

--- src/intro-header.jsx
+++ src/intro-header.jsx
@@ -3,7 +3,7 @@ import popcorn from "../images/popcorn.webp"
 export default function Header(){
     return (
         <header>
-            <img className = "logo" src = {popcorn} alt = "popcorn broken" />
+            <img className = "logo" src = {popcorn} alt = "NO MOVIES" />
             <h1>Vibu.ai</h1>
         </header>
     )
