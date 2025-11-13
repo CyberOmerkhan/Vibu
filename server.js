7ce29078e1319d5c7578adc5b8cab5cc457c7848
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-11-12 19:53:59 -0500

    v0.0.1

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 import http from "http"
+
+const server = http.createServer(async (req, res) => {
+    if(req.url == "/api"){
+        res.end()
+        res.statusCode = 200;
+        res.end("YOOO you've got this")
+    }
+})
