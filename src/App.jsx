7b4bac37708dd9706c4c0b9a581b8224494ebb35
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-11-12 19:43:25 -0500

    v0.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,15 @@ function App() {
           <form className = "intro-inputs">
             <label>
               What's your favorite movie and why?
-              <textarea placeholder = "The Shawshank Redemption because it taught me to never give up hope no matter how hard the life gets"></textarea>
+              <textarea name = "favorite-movie" placeholder = "The Shawshank Redemption because it taught me to never give up hope no matter how hard the life gets"></textarea>
             </label>
             <label>
               Are you in the mood for something new or classic?
-              <input type = "text" placeholder = "I want to watch movies that were released after 1990"></input>
+              <input name = "mood" type = "text" placeholder = "I want to watch movies that were released after 1990"></input>
             </label>
             <label>
               Do you want to have fun or do you want something serious?
-              <input type = "text" placeholder = "I want to watch something stupid and fun"></input>
+              <input name = "genre" type = "text" placeholder = "I want to watch something stupid and fun"></input>
             </label>
             <input className = "form-btn" type = "submit" text = "Submit" />
           </form>
