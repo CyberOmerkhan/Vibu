3cc954ccf67d454842168c66ece9430cf1ce8955
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-11-12 19:57:27 -0500

    v0.0.1

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ import http from "http"
 
 const server = http.createServer(async (req, res) => {
     if(req.url == "/api"){
-        res.end()
         res.statusCode = 200;
         res.end("YOOO you've got this")
     }
+    else{
+        res.statusCode = 404;
+        res.end("Not your best request.")
+    }
 })
+
