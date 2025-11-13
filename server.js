66a1ee2156a4ee11fddb6c0be2085d44b7367e64
CyberOmerkhan <amazingOmerkhan@gmail.com>
2025-11-12 20:08:12 -0500

    v0.0.1

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const PORT = 3000;
 
 const server = http.createServer(async (req, res) => {
     if(req.url == "/api" && req.method == "POST"){
+        let body = "";
+        req.on("data", chunk => {
+            body += chunk.toString(); 
+        });
+        const data = JSON.parse(body);
+        console.log(`received the data: ${data}`);
         res.statusCode = 200;
         res.end("YOOO you've got this")
     }
